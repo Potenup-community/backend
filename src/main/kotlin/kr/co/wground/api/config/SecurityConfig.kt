e06c638fe6599a98fbf82574ef65b83a62977e6a
unknown <dkstpwns10@naver.com>
2025-11-16 22:46:28 +0900

    initial commit

---
+++ src/main/kotlin/kr/co/wground/api/config/SecurityConfig.kt
@@ -0,0 +1,32 @@
+package kr.co.wground.api.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                // (TODO) 경로별 권한 설정
+                it.anyRequest().permitAll()
+            }
+            .formLogin { it.disable() }
+            .httpBasic { it.disable() }
+
+
+            //.oauth2Login { oauth ->
+                // (TODO) Oauth2
+                // 여기에 등록해야 합니다. (지금은 기본 설정 사용)
+                // oauth.userInfoEndpoint { it.userService(customOAuth2UserService) }
+                // oauth.successHandler(oAuth2LoginSuccessHandler)
+            //}
+
+        return http.build()
+    }
+}
