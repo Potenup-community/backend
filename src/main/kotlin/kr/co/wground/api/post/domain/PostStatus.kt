982183c260705a156e9c354723e40f17ff5834be
sanseongKo <tkstjd56@naver.com>
2025-11-20 17:42:57 +0900

    [feature/scrum-4] 포스트 엔티티 타입 변경

---
+++ src/main/kotlin/kr/co/wground/api/post/domain/PostStatus.kt
@@ -0,0 +1,22 @@
+package kr.co.wground.api.post.domain
+
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class PostStatus(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    val viewCount: Int = 0,
+    val commentCount: Int = 0,
+    val likeCount: Int = 0,
+    @Enumerated(EnumType.STRING)
+    val highlightType: HighlightType? = null,
+    val isDeleted: Boolean = false,
+) {
+}
