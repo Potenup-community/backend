951e2d2c7b0a1878ff230499e5fbbe83df4f595a
sanseongKo <tkstjd56@naver.com>
2025-11-20 17:44:54 +0900

    [feature/scrum-4] 포스트 포스트 컨트롤러 추가
    - request 추가
    - writePost 메서드 추가

---
+++ src/main/kotlin/kr/co/wground/api/post/presentaton/PostController.kt
@@ -0,0 +1,21 @@
+package kr.co.wground.api.post.presentaton
+
+import jakarta.validation.Valid
+import kr.co.wground.api.post.application.PostService
+import kr.co.wground.api.post.presentaton.request.PostCreateRequest
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/posts")
+class PostController(
+    private val postService: PostService,
+) {
+
+    @PostMapping
+    fun writePost(@Valid@RequestBody request: PostCreateRequest, writerId: Long = 1): Long {
+        return postService.createPost(request.toDto(writerId))
+    }
+}
