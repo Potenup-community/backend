efdbf8a558739f2d0ba764f876162e3ae6acac15
sanseongKo <tkstjd56@naver.com>
2025-11-20 17:43:40 +0900

    [feature/scrum-4] 포스트 폴더 위치 변경으로 인한 변경

---
+++ src/main/kotlin/kr/co/wground/api/post/domain/HighlightType.kt
@@ -0,0 +1,6 @@
+package kr.co.wground.api.post.domain
+
+enum class HighlightType {
+    BY_ADMIN, BY_LIKE, BY_VIEW
+}
+
---
+++ src/main/kotlin/kr/co/wground/api/post/domain/Topic.kt
@@ -0,0 +1,9 @@
+package kr.co.wground.api.post.domain
+
+enum class Topic(
+    val description: String
+) {
+    CONFERENCE(description = "컨퍼런스"),
+    KNOWLEDGE(description = "지식줍줍"),
+    EMPLOYMENT_TIP(description = "취업팁"),
+}
