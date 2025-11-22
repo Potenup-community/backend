1733d97dad91ee3b15a6e3edaf07cf52dc115109
sanseongKo <tkstjd56@naver.com>
2025-11-22 23:16:11 +0900

    [feature/scrum-4] 폴더명 변경

---
+++ src/main/kotlin/kr/co/wground/post/presentation/PostController.kt
@@ -0,0 +1,28 @@
+package kr.co.wground.post.presentation
+
+import jakarta.validation.Valid
+import kr.co.wground.post.application.PostService
+import kr.co.wground.post.presentation.request.PostCreateRequest
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/posts")
+class PostController(
+    private val postService: PostService,
+) {
+
+    @PostMapping
+    fun writePost(@Valid@RequestBody request: PostCreateRequest, writerId: Long = 1): Long {
+        return postService.createPost(request.toDto(writerId))
+    }
+
+    @DeleteMapping("/{id}")
+    fun deletePost(@PathVariable id: Long) {
+        postService.deletePost(id);
+    }
+}
---
+++ src/main/kotlin/kr/co/wground/post/presentation/request/PostCreateRequest.kt
@@ -0,0 +1,25 @@
+package kr.co.wground.post.presentation.request
+
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
+import kr.co.wground.post.application.dto.PostCreateDto
+import kr.co.wground.post.domain.enums.HighlightType
+import kr.co.wground.post.domain.enums.Topic
+
+data class PostCreateRequest(
+    @field:NotNull(message = "작성할 토픽을 선택해주세요.")
+    val topic: Topic,
+    @field:NotEmpty(message = "제목을 작성해주세요.")
+    val title: String,
+    @field:NotEmpty(message = "본문을 작성해주세요.")
+    val content: String,
+    val highlightType: HighlightType? = null,
+) {
+    fun toDto(writerId: Long) = PostCreateDto(
+        writerId = writerId,
+        topic = topic,
+        title = title,
+        content = content,
+        highlightType = highlightType,
+    )
+}
