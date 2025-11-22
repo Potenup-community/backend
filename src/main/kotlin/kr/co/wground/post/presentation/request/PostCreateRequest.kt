bebcf91f7c5fe4fd2204684e26236b41b1f3d271
sanseongKo <tkstjd56@naver.com>
2025-11-22 23:51:05 +0900

    [feature/scrum-4] valid 될 부분 추가

--- src/main/kotlin/kr/co/wground/post/presentation/request/PostCreateRequest.kt
+++ src/main/kotlin/kr/co/wground/post/presentation/request/PostCreateRequest.kt
@@ -2,6 +2,7 @@ package kr.co.wground.post.presentation.request
 
 import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
 import kr.co.wground.post.application.dto.PostCreateDto
 import kr.co.wground.post.domain.enums.HighlightType
 import kr.co.wground.post.domain.enums.Topic
@@ -10,8 +11,10 @@ data class PostCreateRequest(
     @field:NotNull(message = "작성할 토픽을 선택해주세요.")
     val topic: Topic,
     @field:NotEmpty(message = "제목을 작성해주세요.")
+    @field:Size(max = 50)
     val title: String,
     @field:NotEmpty(message = "본문을 작성해주세요.")
+    @field:Size(max = 5000)
     val content: String,
     val highlightType: HighlightType? = null,
 ) {
