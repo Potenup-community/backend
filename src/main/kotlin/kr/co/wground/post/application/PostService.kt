d5b5190ee62b6dcc80eaba8891886272173cc5bd
sanseongKo <tkstjd56@naver.com>
2025-11-23 01:03:08 +0900

    [feature/scrum-8] 게시글 타이틀, 콘텐츠 수정 서비스 메서드 추가 및 dto 추가

--- src/main/kotlin/kr/co/wground/post/application/PostService.kt
+++ src/main/kotlin/kr/co/wground/post/application/PostService.kt
@@ -1,7 +1,13 @@
 package kr.co.wground.post.application
 
+import kr.co.wground.exception.BusinessException
+import kr.co.wground.like.domain.PostId
 import kr.co.wground.post.application.dto.PostCreateDto
+import kr.co.wground.post.application.dto.PostUpdateDto
+import kr.co.wground.post.domain.Post
+import kr.co.wground.post.exception.PostErrorCode
 import kr.co.wground.post.infra.PostRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -17,5 +23,17 @@ class PostService(
     fun deletePost(id: Long) {
         //TODO(user 자신의 포스트인지 검증 로직 필요)
         postRepository.deleteById(id)
+    }
+
+    fun updatePost(dto: PostUpdateDto) {
+        //TODO(user 자신의 포스트인지 검증 로직 필요)
+        val foundPost = findPostByIdOrThrow(dto.id)
+
+        foundPost.update(dto.title, dto.content)
+    }
+
+    private fun findPostByIdOrThrow(id: PostId): Post {
+        return postRepository.findByIdOrNull(id)
+            ?: throw BusinessException(PostErrorCode.NOT_FOUND_POST)
     }
 }
