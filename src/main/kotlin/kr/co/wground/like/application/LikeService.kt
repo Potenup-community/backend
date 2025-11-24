865fae8481ed6c15876c6fafcddce9a01c3d02c7
dd-jiyun <dlwldbs8015@gmail.com>
2025-11-24 11:47:46 +0900

    [refactor/scrum-8] 좋아요 service transaction 개선

--- src/main/kotlin/kr/co/wground/like/application/LikeService.kt
+++ src/main/kotlin/kr/co/wground/like/application/LikeService.kt
@@ -5,7 +5,6 @@ import kr.co.wground.like.infra.LikeJpaRepository
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.PlatformTransactionManager
-import org.springframework.transaction.TransactionDefinition
 import org.springframework.transaction.support.TransactionTemplate
 
 @Service
@@ -13,18 +12,19 @@ class LikeService(
     private val likeRepository: LikeJpaRepository,
     transactionManager: PlatformTransactionManager,
 ) {
-    private val transactionTemplate = TransactionTemplate(transactionManager).apply {
-        propagationBehavior = TransactionDefinition.PROPAGATION_REQUIRES_NEW
-    }
+    private val transactionTemplate = TransactionTemplate(transactionManager)
 
     fun likePost(dto: LikeCreateDto) {
+        runIdempotently {
+            likeRepository.save(dto.toDomain())
+        }
+    }
+
+    fun runIdempotently(action: () -> Unit) {
         try {
-            transactionTemplate.execute {
-                likeRepository.save(dto.toDomain())
-            }
+            transactionTemplate.execute { action() }
         } catch (_: DataIntegrityViolationException) {
-            // 이미 좋아요를 누른 경우, DB의 유니크 제약 조건으로 인해 예외가 발생할 수 있습니다.
-            // 이 예외는 정상적인 흐름으로 간주하고 무시합니다.
+            // no-op
         }
     }
 }
