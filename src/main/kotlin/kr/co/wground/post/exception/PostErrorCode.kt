9a5f7e7a437d26448f0674ef066c8519c6902cae
sanseongKo <tkstjd56@naver.com>
2025-11-26 16:11:59 +0900

    [feature/scrum-29] Post 본문과 제목 의 불변식 에러코드추가

--- src/main/kotlin/kr/co/wground/post/exception/PostErrorCode.kt
+++ src/main/kotlin/kr/co/wground/post/exception/PostErrorCode.kt
@@ -9,4 +9,7 @@ enum class PostErrorCode(
     override val errorCode: String,
 ): ErrorCode {
     NOT_FOUND_POST("해당 게시글을 찾을 수 없습니다.", HttpStatus.NOT_FOUND, "P-0001"),
+    TITLE_TOO_LONG("게시글의 제목은 50자를 넘을 수 없습니다.", HttpStatus.BAD_REQUEST, "P-0002"),
+    CONTENT_TOO_LONG("게시글의 본문은 5000자를 넘을 수 없습니다.", HttpStatus.BAD_REQUEST, "P-0003"),
+    TITLE_IS_EMPTY("게시글의 제목을 넣어주세요.", HttpStatus.BAD_REQUEST, "P-0004"),
 }
