e14fc51152102e95718551bf184a589b607d2a5d
sanseongKo <tkstjd56@naver.com>
2025-12-17 15:21:53 +0900

    [feature/scrum-52] 포스트 상세 조회 서비스 코드 Dto 추가

---
+++ src/main/kotlin/kr/co/wground/post/application/dto/PostDetailDto.kt
@@ -0,0 +1,32 @@
+package kr.co.wground.post.application.dto
+
+import kr.co.wground.global.common.PostId
+import kr.co.wground.global.common.WriterId
+import kr.co.wground.post.domain.Post
+import kr.co.wground.post.domain.enums.HighlightType
+import kr.co.wground.post.domain.enums.Topic
+import java.time.LocalDateTime
+
+data class PostDetailDto(
+    val postId: PostId,
+    val writerId: WriterId,
+    val writerName: String,
+    val title: String,
+    val content: String,
+    val topic: Topic,
+    val highlightType: HighlightType?,
+    val commentsCount: Int,
+    val wroteAt: LocalDateTime,
+)
+
+fun Post.toDto(writerName: String) = PostDetailDto(
+    postId = id,
+    writerId = writerId,
+    writerName = writerName,
+    title = postBody.title,
+    content = postBody.content,
+    topic = topic,
+    highlightType = postStatus.highlightType,
+    commentsCount = 0,
+    wroteAt = createdAt,
+)
