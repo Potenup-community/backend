6fdad629d7b31e91b749f9ca0377cb211d56f9ed
sanseongKo <tkstjd56@naver.com>
2025-12-20 22:34:15 +0900

    [feature/scrum-79] 이미지 싱크를 맞추기 위한 서비스 코드 추가
    - 마크 다운 코드에서 이미지 url을 가져와 사용하는 이미지와 아닌 이미지를 추려냄
    - 추려낸 것을 기반으로 Delete할지 Used할지 마크
    - 이후 10분 주기 스케줄러로 Deleted 마크된 이미지를 삭제(예정)

--- src/main/kotlin/kr/co/wground/image/application/ImageStorageService.kt
+++ src/main/kotlin/kr/co/wground/image/application/ImageStorageService.kt
@@ -1,34 +1,79 @@
 package kr.co.wground.image.application
 
+import kr.co.wground.common.SyncDraftImagesToPostEvent
 import kr.co.wground.global.common.OwnerId
 import kr.co.wground.image.application.dto.LocalStoredDto
+import kr.co.wground.image.application.dto.UploadImageDto
+import kr.co.wground.image.domain.ImageFile
+import kr.co.wground.image.infra.ImageRepository
 import kr.co.wground.image.policy.UploadPolicy
 import org.springframework.stereotype.Service
-import org.springframework.web.multipart.MultipartFile
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.event.TransactionalEventListener
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardCopyOption
 import java.util.UUID.randomUUID
 
 @Service
+@Transactional
 class ImageStorageService(
-    private val props: UploadPolicy
+    private val props: UploadPolicy,
+    private val imageRepository: ImageRepository
 ) {
-    fun saveTemp(ownerId: OwnerId, file: MultipartFile): LocalStoredDto {
-        val id = randomUUID().toString()
-        val ext = extFromMime(file.contentType) ?: "bin"
+    companion object{
+        private val MD_IMAGE_URL_REGEX = Regex("""!\[[^\]]*]\(([^)]+)\)""")
+    }
 
-        val relativePath = relativePath(ownerId, id, ext)
+    fun saveTemp(dto: UploadImageDto): LocalStoredDto {
+        val id = randomUUID().toString()
+        val ext = extFromMime(dto.imageFile.contentType) ?: "bin"
+
+        val relativePath = relativePath(dto.ownerId, id, ext)
         val targetPath = Path.of(props.localDir, relativePath)
 
         Files.createDirectories(targetPath.parent)
 
-        file.inputStream.use { input ->
+        dto.imageFile.inputStream.use { input ->
             Files.copy(input, targetPath, StandardCopyOption.REPLACE_EXISTING)
         }
 
         val url = uploadedUrl(relativePath)
+
+        val imageFile = ImageFile.create(dto.ownerId, dto.draftId, relativePath)
+        imageRepository.save(imageFile)
+
         return LocalStoredDto(id = id, relativePath = relativePath, url = url)
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @TransactionalEventListener
+    fun syncDraftImageFiles(dto: SyncDraftImagesToPostEvent) {
+        val urls = extractUploadUrls(dto.markdown)
+        val usedRelativePaths = urls.map { toRelativePath(it) }.toSet()
+
+        val currentlyLinked = imageRepository.findAllByDraftIdAndOwnerId(dto.draftId, dto.ownerId)
+
+        currentlyLinked.filter { it.relativePath in usedRelativePaths }
+            .forEach {
+                it.markUsed()
+                it.fillPostId(dto.postId)
+            }
+
+        currentlyLinked.filter { it.relativePath !in usedRelativePaths }
+            .forEach {it.markOrphan()}
+    }
+
+    private fun extractUploadUrls(markdown: String): Set<String> {
+        return MD_IMAGE_URL_REGEX.findAll(markdown)
+            .map { it.groupValues[1].trim() }
+            .filter { it.startsWith(props.publicBasePath) }
+            .toSet()
+    }
+
+    private fun toRelativePath(url: String): String {
+        return url.removePrefix(props.publicBasePath).trimStart('/') // tmp/1/uuid.jpg
     }
 
     private fun uploadedUrl(relativePath: String): String = "${props.publicBasePath.trimEnd('/')}/$relativePath"
