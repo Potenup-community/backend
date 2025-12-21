f28e49af93cfed5857447814b5ec3549824188aa
SeungYeop-Han <ammezkhan@gmail.com>
2025-12-21 10:58:26 +0900

    refactor(reaction): 요청 dto validation 추가

--- src/main/kotlin/kr/co/wground/reaction/presentation/request/ReactionRequest.kt
+++ src/main/kotlin/kr/co/wground/reaction/presentation/request/ReactionRequest.kt
@@ -1,5 +1,7 @@
 package kr.co.wground.reaction.presentation.request
 
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
 import kr.co.wground.global.common.UserId
 import kr.co.wground.reaction.application.dto.CommentReactCommand
 import kr.co.wground.reaction.application.dto.PostReactCommand
@@ -8,9 +10,17 @@ import kr.co.wground.reaction.presentation.request.ReactionTarget.COMMENT
 import kr.co.wground.reaction.presentation.request.ReactionTarget.POST
 
 data class ReactionRequest(
+
+    @field:NotNull(message = "반응할 대상의 유형을 명시해주세요(ex. POST, COMMENT, ...)")
     val targetType: ReactionTarget,
+
+    @field:NotNull(message = "targetId 가 null 입니다.")
+    @field:Positive(message = "대상의 id 는 0 또는 음수일 수 없습니다.")
     val targetId: Long,
+
+    @field:NotNull(message = "반응 유형을 명시해주세요(ex. LIKE, HEART, ...)")
     val reactionType: ReactionType,
+
 ) {
     fun toPostReactCommand(userId: UserId) : PostReactCommand {
         if (targetType != POST) {
