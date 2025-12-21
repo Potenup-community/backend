ce88053e022e088508eaffd385d4a37d181d9c96
SeungYeop-Han <ammezkhan@gmail.com>
2025-12-21 11:29:44 +0900

    feat(reaction): 요청 핸들러 구현
    + 불필요한 세미콜론 제거

--- src/main/kotlin/kr/co/wground/reaction/presentation/ReactionController.kt
+++ src/main/kotlin/kr/co/wground/reaction/presentation/ReactionController.kt
@@ -1,24 +1,33 @@
 package kr.co.wground.reaction.presentation
 
 import jakarta.validation.Valid
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
 import kr.co.wground.global.common.PostId
 import kr.co.wground.global.config.resolver.CurrentUserId
 import kr.co.wground.reaction.application.ReactionCommandService
+import kr.co.wground.reaction.application.ReactionQueryService
 import kr.co.wground.reaction.presentation.request.ReactionRequest
 import kr.co.wground.reaction.presentation.request.ReactionTarget.COMMENT
 import kr.co.wground.reaction.presentation.request.ReactionTarget.POST
+import kr.co.wground.reaction.application.dto.PostReactionStats
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/reactions")
 class ReactionController(
     private val reactionCommandService: ReactionCommandService,
+    private val reactionQueryService: ReactionQueryService,
 ) {
+
+    // commands --------------------
 
     @PostMapping
     fun react(
@@ -26,7 +35,7 @@ class ReactionController(
         user: CurrentUserId,
     ): ResponseEntity<Unit> {
 
-        val userId = user.value;
+        val userId = user.value
 
         when (request.targetType) {
             POST -> reactionCommandService.reactToPost(request.toPostReactCommand(userId))
@@ -42,7 +51,7 @@ class ReactionController(
         user: CurrentUserId,
     ): ResponseEntity<Unit> {
 
-        val userId = user.value;
+        val userId = user.value
 
         when (request.targetType) {
             POST -> reactionCommandService.unreactToPost(request.toPostReactCommand(userId))
@@ -50,5 +59,24 @@ class ReactionController(
         }
 
         return ResponseEntity.noContent().build()
+    }
+
+    // queries --------------------
+
+    @GetMapping("/posts")
+    fun getPostReactionStats(
+        @NotNull(message = "postId 가 null 입니다.")
+        @Positive(message = "postId 는 0 또는 음수일 수 없습니다.")
+        @RequestParam
+        postId: PostId,
+        user: CurrentUserId
+    ): ResponseEntity<PostReactionStats> {
+
+        val userId = user.value
+
+        // To Do: 서비스 쿼리 결과를 담는 dto 를 XxxResponse Dto 로 매핑하는 로직 추가
+        val result = reactionQueryService.getPostReactionStats(userId, postId)
+
+        return ResponseEntity.ok(result)
     }
 }
