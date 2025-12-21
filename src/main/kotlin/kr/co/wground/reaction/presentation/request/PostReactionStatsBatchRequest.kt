44f5e1c350338d15aba5d855409fcb134326a60e
SeungYeop-Han <ammezkhan@gmail.com>
2025-12-21 16:06:46 +0900

    feat(reaction): 여러 post reaction stat 한 번에 요청하는 요청 바디 dto 정의

---
+++ src/main/kotlin/kr/co/wground/reaction/presentation/request/PostReactionStatsBatchRequest.kt
@@ -0,0 +1,12 @@
+package kr.co.wground.reaction.presentation.request
+
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+import kr.co.wground.global.common.PostId
+
+data class PostReactionStatsBatchRequest(
+    @field:NotNull(message = "빈 postId 집합")
+    // To Do: 나중에 매직 넘버 상수로 뺄 계획입니다.
+    @field:Size(min = 1, max = 50, message = "postId 집합이 너무 작거나 큽니다. 1 이상 50 이하여야 합니다.")
+    val postIds: Set<PostId>
+)
