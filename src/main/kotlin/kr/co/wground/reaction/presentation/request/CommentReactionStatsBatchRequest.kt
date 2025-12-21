ec248531cb4f14860e82632630aff6027e36b455
SeungYeop-Han <ammezkhan@gmail.com>
2025-12-21 17:26:33 +0900

    refactor(reaction): 에러 메시지 어조 통일

--- src/main/kotlin/kr/co/wground/reaction/presentation/request/CommentReactionStatsBatchRequest.kt
+++ src/main/kotlin/kr/co/wground/reaction/presentation/request/CommentReactionStatsBatchRequest.kt
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size
 import kr.co.wground.global.common.CommentId
 
 data class CommentReactionStatsBatchRequest(
-    @field:NotNull(message = "빈 commentId 집합")
+    @field:NotNull(message = "commentId 집합이 비어있습니다.")
     // To Do: 나중에 매직 넘버 상수로 뺄 계획입니다.
     @field:Size(min = 1, max = 50, message = "commentId 집합이 너무 작거나 큽니다. 1 이상 50 이하여야 합니다.")
     val commentIds: Set<CommentId>
