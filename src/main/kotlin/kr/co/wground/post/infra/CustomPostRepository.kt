6447ae3fd9b9db60d8f39b833c695de828788342
sanseongKo <tkstjd56@naver.com>
2025-12-22 15:24:22 +0900

    [feature/scrum-91] 게시판 서머리 조회 topic 기반 필터링 추가 및 함수 네이밍 변경

--- src/main/kotlin/kr/co/wground/post/infra/CustomPostRepository.kt
+++ src/main/kotlin/kr/co/wground/post/infra/CustomPostRepository.kt
@@ -1,9 +1,9 @@
 package kr.co.wground.post.infra
 
 import kr.co.wground.post.domain.Post
-import org.springframework.data.domain.Pageable
+import kr.co.wground.post.infra.predicate.GetPostSummaryPredicate
 import org.springframework.data.domain.SliceImpl
 
 interface CustomPostRepository {
-    fun findAllByPageable(pageable: Pageable): SliceImpl<Post>
+    fun findAllByPredicate(predicate: GetPostSummaryPredicate): SliceImpl<Post>
 }
