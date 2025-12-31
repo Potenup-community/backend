fdee13938f810610cab8c2fdb89962635950d73d
SeungYeop-Han <ammezkhan@gmail.com>
2025-12-31 12:03:12 +0900

    feature(monitoring): 요청 별 로그 추적을 위한 traceId 의존성 도입

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("io.micrometer:micrometer-registry-prometheus")
+	implementation("io.micrometer:micrometer-tracing-bridge-brave")
 
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
