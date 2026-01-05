b37bf27a12aab27b29f99d1f7cc61adafb977e19
SeungYeop-Han <ammezkhan@gmail.com>
2026-01-05 10:48:31 +0900

    feature(monitoring): micrometer-tracing-bridge 구현체 변경 brave -> otel

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("io.micrometer:micrometer-registry-prometheus")
-	implementation("io.micrometer:micrometer-tracing-bridge-brave")
+	implementation("io.micrometer:micrometer-tracing-bridge-otel")
 
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
