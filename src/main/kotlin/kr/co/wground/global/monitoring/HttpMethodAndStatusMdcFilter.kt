a22b1f8a66ed608eb40758456b7f5bd25ea60c9d
SeungYeop-Han <ammezkhan@gmail.com>
2026-01-05 13:27:22 +0900

    feat(monitoring): HTTP 응답 코드 및 소요 시간
    해당 정보를 요청 당 한 번만 로그에 찍히도록 만들기 위해,
    요청의 처리가 완료되고 상태 코드가 결정된 시점에서 필터 쪽에서 응답이 나가기 전 로그를 찍는다.

--- src/main/kotlin/kr/co/wground/global/monitoring/HttpMethodAndStatusMdcFilter.kt
+++ src/main/kotlin/kr/co/wground/global/monitoring/HttpMethodAndStatusMdcFilter.kt
@@ -4,22 +4,34 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.jboss.logging.MDC
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
 @Component
 class HttpMethodAndStatusMdcFilter : OncePerRequestFilter() {
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
+
+        val beginTimeNs = System.nanoTime()
+
         try {
             MDC.put("http_method", request.method)
             filterChain.doFilter(request, response)
         } finally {
+            val elapsedTimeMs = (System.nanoTime() - beginTimeNs) / 1_000_000
             MDC.put("http_status", response.status.toString())
+            MDC.put("elapsed_time_ms", elapsedTimeMs)
+
+            // 응답 이후 로그를 한 번 찍어서 http status 및 요청 처리 시간 기록
+            logger.info("HTTP_RESPONSE_LOG")
+
             MDC.clear() // 누수 방지를 위해
         }
     }
