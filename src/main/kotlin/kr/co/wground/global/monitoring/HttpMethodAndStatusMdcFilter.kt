02bfff4769611abe56c50361292efb8c1c38d082
SeungYeop-Han <ammezkhan@gmail.com>
2026-01-05 11:47:36 +0900

    feat(monitoring): MDC http.method 및 http.status 구현
    스프링 필터 기반

---
+++ src/main/kotlin/kr/co/wground/global/monitoring/HttpMethodAndStatusMdcFilter.kt
@@ -0,0 +1,26 @@
+package kr.co.wground.global.monitoring
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.jboss.logging.MDC
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+class HttpMethodAndStatusMdcFilter : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        try {
+            MDC.put("http.method", request.method)
+            filterChain.doFilter(request, response)
+        } finally {
+            MDC.put("http.status", response.status.toString())
+            MDC.clear() // 누수 방지를 위해
+        }
+    }
+}
