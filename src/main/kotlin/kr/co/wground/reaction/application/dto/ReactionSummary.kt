44d1422115c1d73e5bcc46d030d66f315c798999
SeungYeop-Han <ammezkhan@gmail.com>
2026-01-11 15:27:40 +0900

    feat(reaction): 응답 dto 에 스웨거 붙이기
    단 해당 응답 dto 는 서비스 계층에서 반환되는 것을 매핑 없이 그대로 사용하고 있음
    
    매핑 로직을 한 계층 더 거치는 것이 무의미하다고 판단했기 때문임
    
    그래도 해당 부분을 추후에 수정해야 할 수도 있으며 이에 대해 인지는 하고 있음

--- src/main/kotlin/kr/co/wground/reaction/application/dto/ReactionSummary.kt
+++ src/main/kotlin/kr/co/wground/reaction/application/dto/ReactionSummary.kt
@@ -1,7 +1,19 @@
 package kr.co.wground.reaction.application.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "리액션 횟수 및 사용자에 의해 리액션 되었는 지의 여부")
 data class ReactionSummary(
+    @field:Schema(
+        description = "해당 타입 반응 수",
+        example = "3",
+    )
     val count: Int = 0,
+
+    @field:Schema(
+        description = "현재 로그인 사용자가 해당 타입으로 반응했는지 여부",
+        example = "true"
+    )
     val reactedByMe: Boolean
 ) {
 }
