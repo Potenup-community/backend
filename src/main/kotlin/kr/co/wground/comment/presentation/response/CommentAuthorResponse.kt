d9befcffa7747d6fef60af2d0f4277797b5ffe19
dd-jiyun <dlwldbs8015@gmail.com>
2026-01-12 14:28:56 +0900

    [refactor/scrum-121] 변경 사항에 맞게 Swagger 필드 설정 추가

--- src/main/kotlin/kr/co/wground/comment/presentation/response/CommentAuthorResponse.kt
+++ src/main/kotlin/kr/co/wground/comment/presentation/response/CommentAuthorResponse.kt
@@ -1,10 +1,15 @@
 package kr.co.wground.comment.presentation.response
 
+import io.swagger.v3.oas.annotations.media.Schema
 import kr.co.wground.global.common.UserId
 
 data class CommentAuthorResponse(
+    @field:Schema(description = "댓글 작성자 ID")
     val userId: UserId,
+    @field:Schema(description = "댓글 작성자 이름")
     val name: String,
+    @field:Schema(description = "댓글 작성자 트랙 이름")
     val trackName: String,
+    @field:Schema(description = "댓글 작성자 프로필 이미지 URL")
     val profileImageUrl: String,
 )
--- src/main/kotlin/kr/co/wground/comment/presentation/response/CommentsResponse.kt
+++ src/main/kotlin/kr/co/wground/comment/presentation/response/CommentsResponse.kt
@@ -1,5 +1,8 @@
 package kr.co.wground.comment.presentation.response
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class CommentsResponse(
+    @field:Schema(description = "댓글 목록")
     val contents: List<CommentSummaryResponse>,
 )
