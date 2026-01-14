193fee6d2bce98f15b3df45ed74487bd8150a532
dkstpwns10 <dkstpwns10@naver.com>
2026-01-14 19:11:16 +0900

    [feature/scrum-137] Test코드 수정 및 Swagger추가

--- src/main/kotlin/kr/co/wground/user/presentation/ProfileApi.kt
+++ src/main/kotlin/kr/co/wground/user/presentation/ProfileApi.kt
@@ -57,7 +57,69 @@ interface ProfileApi {
         @Parameter(hidden = true) request: WebRequest
     ): ResponseEntity<ProfileResponse>
 
+
+    @Operation(
+        summary = "프로필 이미지 저장",
+        description = "유저 ID에 업로드한 프로필 이미지를 저장합니다."
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "204",
+                description = "프로필 이미지 저장 성공",
+                content = [Content(mediaType = "image/jpeg", schema = Schema(type = "string", format = "binary"))]
+            ),
+            ApiResponse(
+                responseCode = "304",
+                description = "변경 없음 (캐시 유지)"
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "유저를 찾을 수 없음",
+                content = [Content(
+                    mediaType = "application/json",
+                    schema = Schema(implementation = ErrorResponse::class),
+                    examples = [
+                        ExampleObject(name = "USER_NOT_FOUND", value = UserSwaggerErrorExample.NotFound.USER_NOT_FOUND),
+                        ExampleObject(
+                            name = "PROFILE_NOT_FOUND",
+                            value = UserSwaggerErrorExample.NotFound.PROFILE_NOT_FOUND
+                        )
+                    ]
+                )]
+            )
+        ]
+    )
     fun uploadProfileImage(@RequestPart("file") file: MultipartFile, userId: CurrentUserId): ResponseEntity<Unit>
 
+
+    @Operation(
+        summary = "프로필 이미지 삭제",
+        description = "유저 ID에 해당하는 프로필 이미지를 삭제합니다."
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "204",
+                description = "프로필 이미지 삭제 성공",
+                content = [Content(mediaType = "image/jpeg", schema = Schema(type = "string", format = "binary"))]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "유저를 찾을 수 없음",
+                content = [Content(
+                    mediaType = "application/json",
+                    schema = Schema(implementation = ErrorResponse::class),
+                    examples = [
+                        ExampleObject(name = "USER_NOT_FOUND", value = UserSwaggerErrorExample.NotFound.USER_NOT_FOUND),
+                        ExampleObject(
+                            name = "PROFILE_NOT_FOUND",
+                            value = UserSwaggerErrorExample.NotFound.PROFILE_NOT_FOUND
+                        )
+                    ]
+                )]
+            )
+        ]
+    )
     fun deleteProfile(userId: CurrentUserId): ResponseEntity<Unit>
 }
