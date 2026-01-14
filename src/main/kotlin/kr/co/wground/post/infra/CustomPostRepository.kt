95c7440493f4224922fd34a71d65b8dddd5903d2
sanseongKo <tkstjd56@naver.com>
2026-01-14 19:12:03 +0900

    [feature/scrum-142] 좋아요가 존재하는지 확인하는 sub query가 포함된 게시글 조회 쿼리 추가

--- src/main/kotlin/kr/co/wground/post/infra/CustomPostRepository.kt
+++ src/main/kotlin/kr/co/wground/post/infra/CustomPostRepository.kt
@@ -1,9 +1,12 @@
 package kr.co.wground.post.infra
 
+import kr.co.wground.global.common.UserId
 import kr.co.wground.post.domain.Post
 import kr.co.wground.post.infra.predicate.GetPostSummaryPredicate
-import org.springframework.data.domain.SliceImpl
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 
 interface CustomPostRepository {
-    fun findAllByPredicate(predicate: GetPostSummaryPredicate): SliceImpl<Post>
+    fun findAllByPredicate(predicate: GetPostSummaryPredicate): Slice<Post>
+    fun findAllLikedByUser(userId: UserId, pageable: Pageable): Slice<Post>
 }
--- src/main/kotlin/kr/co/wground/post/infra/CustomPostRepositoryImpl.kt
+++ src/main/kotlin/kr/co/wground/post/infra/CustomPostRepositoryImpl.kt
@@ -1,14 +1,19 @@
 package kr.co.wground.post.infra
 
 import com.querydsl.core.types.OrderSpecifier
+import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.core.types.dsl.Expressions
 import com.querydsl.core.types.dsl.NumberExpression
+import com.querydsl.jpa.JPAExpressions
 import com.querydsl.jpa.impl.JPAQueryFactory
 import kr.co.wground.global.common.UserId
 import kr.co.wground.post.domain.Post
 import kr.co.wground.post.domain.QPost.post
 import kr.co.wground.post.domain.enums.Topic
 import kr.co.wground.post.infra.predicate.GetPostSummaryPredicate
+import kr.co.wground.reaction.domain.QPostReaction.postReaction
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 import org.springframework.data.domain.SliceImpl
 import org.springframework.data.domain.Sort
 
@@ -29,7 +34,10 @@ class CustomPostRepositoryImpl(private val jpaQueryFactory: JPAQueryFactory): Cu
                 eqTopic(predicate.topic),
                 eqUserId(predicate.userId)
             )
-            .orderBy(*toOrderSpecifiers(pageable.sort).toTypedArray())
+            .orderBy(
+                *toOrderSpecifiers(pageable.sort).toTypedArray(),
+                post.id.desc()
+            )
             .offset(pageable.offset)
             .limit(pageable.pageSize.toLong() + 1)
             .fetch()
@@ -41,6 +49,43 @@ class CustomPostRepositoryImpl(private val jpaQueryFactory: JPAQueryFactory): Cu
             pageable,
             hasNext
         )
+    }
+
+    override fun findAllLikedByUser(
+        userId: UserId,
+        pageable: Pageable,
+    ): Slice<Post> {
+        val content = jpaQueryFactory
+            .selectFrom(post)
+            .where(
+                existsLikedBy(userId),
+            )
+            .orderBy(
+                post.createdAt.desc(),
+                post.id.desc(),
+            )
+            .offset(pageable.offset)
+            .limit(pageable.pageSize.toLong() + 1)
+            .fetch()
+
+        val hasNext = content.size > pageable.pageSize
+
+        return SliceImpl(
+            content.take(pageable.pageSize),
+            pageable,
+            hasNext
+        )
+    }
+
+    private fun existsLikedBy(userId: UserId): BooleanExpression {
+        return JPAExpressions
+            .selectOne()
+            .from(postReaction)
+            .where(
+                postReaction.userId.eq(userId),
+                postReaction.postId.eq(post.id),
+            )
+            .exists()
     }
 
     private data class PopularityParams(
