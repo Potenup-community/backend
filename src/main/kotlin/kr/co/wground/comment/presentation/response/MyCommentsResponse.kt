0debfd13826fd29f694fa44eb97122e49bd9b8e2
dd-jiyun <dlwldbs8015@gmail.com>
2026-01-18 17:32:33 +0900

    [refactor/scrum-151] 서비스 로직 수정 및 필요한 데이터에 맞게 response 변경

--- src/main/kotlin/kr/co/wground/comment/presentation/response/MyCommentsResponse.kt
+++ src/main/kotlin/kr/co/wground/comment/presentation/response/MyCommentsResponse.kt
@@ -2,7 +2,7 @@ package kr.co.wground.comment.presentation.response
 
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
-import kr.co.wground.comment.application.dto.CommentSummaryDto
+import kr.co.wground.comment.application.dto.MyCommentSummaryDto
 import kr.co.wground.global.common.CommentId
 import kr.co.wground.global.common.PostId
 import org.springframework.data.domain.Slice
@@ -20,33 +20,31 @@ data class MyCommentItemResponse(
     val postId: PostId,
     @field:Schema(description = "댓글 내용")
     val content: String,
-    @field:Schema(description = "댓글 작성자 정보")
-    val author: CommentAuthorResponse,
     @field:Schema(description = "댓글 작성 시간")
     val createdAt: LocalDateTime,
+    @field:Schema(description = "게시글 조회수")
+    val viewCount: Int,
+    @field:Schema(description = "댓글 좋아요수")
+    val likeCount: Int,
     @field:Schema(description = "댓글 삭제 여부")
     val isDeleted: Boolean,
 ) {
     companion object {
-        fun from(dto: CommentSummaryDto): MyCommentItemResponse {
+        fun from(dto: MyCommentSummaryDto): MyCommentItemResponse {
             return MyCommentItemResponse(
                 commentId = dto.commentId,
                 postId = dto.postId,
                 content = dto.content,
-                author = CommentAuthorResponse(
-                    userId = dto.authorId,
-                    name = dto.authorName,
-                    trackName = dto.trackName,
-                    profileImageUrl = dto.authorProfileImageUrl,
-                ),
                 createdAt = dto.createdAt,
+                viewCount = dto.viewCount,
+                likeCount = dto.likeCount,
                 isDeleted = dto.isDeleted,
             )
         }
     }
 }
 
-fun Slice<CommentSummaryDto>.toResponse(): MyCommentsResponse {
+fun Slice<MyCommentSummaryDto>.toResponse(): MyCommentsResponse {
     return MyCommentsResponse(
         contents = this.content.map { MyCommentItemResponse.from(it) },
         hasNext = this.hasNext(),
