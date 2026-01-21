46582983d82ee91244983f74c132f75791103141
dkstpwns10 <dkstpwns10@naver.com>
2026-01-22 02:41:06 +0900

    [feature/scrum-185] Tag엔티티 도메인 로직 구현

--- src/main/kotlin/kr/co/wground/study/domain/Tag.kt
+++ src/main/kotlin/kr/co/wground/study/domain/Tag.kt
@@ -5,14 +5,51 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import kr.co.wground.exception.BusinessException
+import kr.co.wground.study.domain.exception.StudyDomainErrorCode
 
 @Entity
-class Tag (
+class Tag protected constructor(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
     @Column(unique = true, nullable = false)
     val name: String
-){
+) {
+    companion object {
+        private const val MIN_LENGTH = 2
+        private const val MAX_LENGTH = 15
+        private val SPECIAL_CHAR_LIST = listOf('+', '#', '.', '_', '-')
+        private val VALID_PATTERN = Regex("^[가-힣a-z0-9+#._-]+$")
 
+        fun create(rawName: String): Tag {
+            val normalizedName = normalize(rawName)
+            validateTag(normalizedName)
+            return Tag(name = normalizedName)
+        }
+
+        private fun normalize(input: String): String {
+            return input.trim()
+                .lowercase()
+                .replace(" ", "").replace(VALID_PATTERN, "")
+                .filter {
+                    it.isDigit() ||
+                            it.isLowerCase() ||
+                            it in '가'..'힣' ||
+                            it in SPECIAL_CHAR_LIST
+                }
+        }
+
+        private fun validateTag(name: String) {
+            if (name.isBlank()) {
+                throw BusinessException(StudyDomainErrorCode.TAG_FORMAT_INVALID)
+            }
+            if (name.length !in MIN_LENGTH..MAX_LENGTH) {
+                throw BusinessException(StudyDomainErrorCode.TAG_LENGTH_INVALID_RANGE)
+            }
+            if (!VALID_PATTERN.matches(name)) {
+                throw BusinessException(StudyDomainErrorCode.TAG_FORMAT_INVALID)
+            }
+        }
+    }
 }
--- src/main/kotlin/kr/co/wground/study/domain/exception/StudyDomainErrorCode.kt
+++ src/main/kotlin/kr/co/wground/study/domain/exception/StudyDomainErrorCode.kt
@@ -8,7 +8,13 @@ enum class StudyDomainErrorCode(
     override val code: String,
     override val message: String,
 ) : ErrorCode {
+    //StudySchedule
     STUDY_CANT_START_AFTER_END_DATE(HttpStatus.BAD_REQUEST,"S-0001","모집 시작은 모집 종료보다 빨라야 합니다."),
     STUDY_RECRUIT_COMPLETE_BEFORE_END_DATE(HttpStatus.BAD_REQUEST,"S-0002","모집 종료는 스터디 종료보다 빨라야 합니다."),
     STUDY_MONTH_ILLEGAL_RANGE(HttpStatus.BAD_REQUEST,"S-0003","유효하지 않은 차수입니다. 1~5 사이여야 합니다)."),
+
+    //Tag
+    TAG_LENGTH_INVALID_RANGE(HttpStatus.BAD_REQUEST,"TG-0001", "태그의 길이가 양식에 맞지 않습니다."),
+    TAG_FORMAT_INVALID(HttpStatus.BAD_REQUEST,"TG-0002","태그의 입력 양식이 올바르지 않습니다.")
+
 }
