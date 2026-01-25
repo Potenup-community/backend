27e5107e31b11b1be52096114d9ec1ed0f7be80a
dd-jiyun <dlwldbs8015@gmail.com>
2026-01-25 22:44:11 +0900

    [feature/scrum-188] 멘션 이벤트 멘션된 사용자 변수명 변경

--- src/main/kotlin/kr/co/wground/common/event/MentionCreatedEvent.kt
+++ src/main/kotlin/kr/co/wground/common/event/MentionCreatedEvent.kt
@@ -6,5 +6,5 @@ import kr.co.wground.global.common.UserId
 data class MentionCreatedEvent(
     val commentId: CommentId,
     val mentionerId: UserId, // 멘션"한" 사용자
-    val mentionedUserIds: List<UserId>, // 멘션"된" 사용자
+    val mentionUserIds: List<UserId>, // 멘션"된" 사용자
 )
