2513bbd60cca9b1b3f087d9c585d1e83a4b89022
SeungYeop-Han <ammezkhan@gmail.com>
2026-01-27 09:36:20 +0900

    test-refactor(study): 테스트 warning 해소

--- src/test/kotlin/kr/co/wground/study/domain/StudyTagTest.kt
+++ src/test/kotlin/kr/co/wground/study/domain/StudyTagTest.kt
@@ -3,7 +3,6 @@ package kr.co.wground.study.domain
 import kr.co.wground.exception.BusinessException
 import kr.co.wground.study.domain.exception.StudyDomainErrorCode
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.lang.reflect.Modifier
@@ -12,82 +11,75 @@ import kotlin.test.assertEquals
 @DisplayName("태그(Tag) 테스트")
 class StudyTagTest {
 
-    @Nested
-    @DisplayName("내부 요구조건 테스트")
-    inner class TagInternalConditionTest {
+    @Test
+    fun `태그 생성 시, 공백 문자와 허용되지 않은 특수 문자가 제외되고, 대문자가 소문자로 변환된다`() {
+        val created = Tag.create(rawName = "  \t  +#._-*****ABC******가나다 \n   ")
 
-        @Test
-        fun `태그 생성 시, 공백 문자와 허용되지 않은 특수 문자가 제외되고, 대문자가 소문자로 변환된다`() {
-            val created = Tag.create(rawName = "  \t  +#._-*****ABC******가나다 \n   ")
-            
-            assertEquals("+#._-abc가나다", created.name)
+        assertEquals("+#._-abc가나다", created.name)
+    }
+
+    @Test
+    fun `태그 생성 시, 태그 이름이 공백 문자를 제외하고 비어있는 경우, 예외 발생 - BusinessException(TG-0001)`() {
+        val thrown = assertThrows<BusinessException> {
+            Tag.create(rawName = "\n  \t")
         }
 
-        @Test
-        fun `태그 생성 시, 태그 이름이 공백 문자를 제외하고 비어있는 경우, 예외 발생 - BusinessException(TG-0001)`() {
-            val thrown = assertThrows<BusinessException> {
-                val created = Tag.create(rawName = "\n  \t")
-            }
+        assertEquals(StudyDomainErrorCode.TAG_FORMAT_INVALID.code, thrown.code)
+    }
 
-            assertEquals(StudyDomainErrorCode.TAG_FORMAT_INVALID.code, thrown.code)
+    @Test
+    fun `태그 생성 시, 공백 문자를 제외한 태그 이름의 길이가 MIN_LENGTH 보다 작은 경우, 예외 발생 - BusinessException(TG-0002)`() {
+
+        val MIN_LENGTH = extractMinLengthFromTagClass()
+
+        val thrown = assertThrows<BusinessException> {
+            Tag.create(rawName = "   " + "a".repeat(MIN_LENGTH - 1) + "\t\n")
         }
 
-        @Test
-        fun `태그 생성 시, 공백 문자를 제외한 태그 이름의 길이가 MIN_LENGTH 보다 작은 경우, 예외 발생 - BusinessException(TG-0002)`() {
+        assertEquals(StudyDomainErrorCode.TAG_LENGTH_INVALID_RANGE.code, thrown.code)
+    }
 
-            val MIN_LENGTH = extractMinLengthFromTagClass()
-            println("MIN_LENGTH = ${MIN_LENGTH}")
+    @Test
+    fun `태그 생성 시, 공백 문자를 제외한 태그 이름의 길이가 MAX_LENGTH 보다 큰 경우, 예외 발생 - BusinessException(TG-0002)`() {
 
-            val thrown = assertThrows<BusinessException> {
-                val created = Tag.create(rawName = "   " + "a".repeat(MIN_LENGTH - 1) + "\t\n")
-            }
+        val MAX_LENGTH = extractMaxLengthFromTagClass()
 
-            assertEquals(StudyDomainErrorCode.TAG_LENGTH_INVALID_RANGE.code, thrown.code)
+        val thrown = assertThrows<BusinessException> {
+            Tag.create(rawName = "   " + "a".repeat(MAX_LENGTH + 1) + "\t\n")
         }
 
-        @Test
-        fun `태그 생성 시, 공백 문자를 제외한 태그 이름의 길이가 MAX_LENGTH 보다 큰 경우, 예외 발생 - BusinessException(TG-0002)`() {
+        assertEquals(StudyDomainErrorCode.TAG_LENGTH_INVALID_RANGE.code, thrown.code)
+    }
 
-            val MAX_LENGTH = extractMaxLengthFromTagClass()
-            println("MAX_LENGTH = ${MAX_LENGTH}")
+    // ----- helpers
 
-            val thrown = assertThrows<BusinessException> {
-                val created = Tag.create(rawName = "   " + "a".repeat(MAX_LENGTH + 1) + "\t\n")
-            }
+    private fun extractMinLengthFromTagClass(): Int {
 
-            assertEquals(StudyDomainErrorCode.TAG_LENGTH_INVALID_RANGE.code, thrown.code)
+        runCatching {
+            val f = Tag::class.java.getDeclaredField("MIN_LENGTH").apply { isAccessible = true }
+            return if (Modifier.isStatic(f.modifiers)) f.getInt(null) else f.getInt(null) // 거의 static
         }
 
-        // ----- helpers
+        val companionInstance = Tag::class.java.getDeclaredField("Companion")
+            .apply { isAccessible = true }
+            .get(null) // static 필드라 null
 
-        private fun extractMinLengthFromTagClass(): Int {
+        val f = companionInstance.javaClass.getDeclaredField("MIN_LENGTH").apply { isAccessible = true }
+        return if (Modifier.isStatic(f.modifiers)) f.getInt(null) else f.getInt(companionInstance)
+    }
 
-            runCatching {
-                val f = Tag::class.java.getDeclaredField("MIN_LENGTH").apply { isAccessible = true }
-                return if (Modifier.isStatic(f.modifiers)) f.getInt(null) else f.getInt(null) // 거의 static
-            }
+    private fun extractMaxLengthFromTagClass(): Int {
 
-            val companionInstance = Tag::class.java.getDeclaredField("Companion")
-                .apply { isAccessible = true }
-                .get(null) // static 필드라 null
-
-            val f = companionInstance.javaClass.getDeclaredField("MIN_LENGTH").apply { isAccessible = true }
-            return if (Modifier.isStatic(f.modifiers)) f.getInt(null) else f.getInt(companionInstance)
+        runCatching {
+            val f = Tag::class.java.getDeclaredField("MAX_LENGTH").apply { isAccessible = true }
+            return if (Modifier.isStatic(f.modifiers)) f.getInt(null) else f.getInt(null) // 거의 static
         }
 
-        private fun extractMaxLengthFromTagClass(): Int {
+        val companionInstance = Tag::class.java.getDeclaredField("Companion")
+            .apply { isAccessible = true }
+            .get(null) // static 필드라 null
 
-            runCatching {
-                val f = Tag::class.java.getDeclaredField("MAX_LENGTH").apply { isAccessible = true }
-                return if (Modifier.isStatic(f.modifiers)) f.getInt(null) else f.getInt(null) // 거의 static
-            }
-
-            val companionInstance = Tag::class.java.getDeclaredField("Companion")
-                .apply { isAccessible = true }
-                .get(null) // static 필드라 null
-
-            val f = companionInstance.javaClass.getDeclaredField("MAX_LENGTH").apply { isAccessible = true }
-            return if (Modifier.isStatic(f.modifiers)) f.getInt(null) else f.getInt(companionInstance)
-        }
+        val f = companionInstance.javaClass.getDeclaredField("MAX_LENGTH").apply { isAccessible = true }
+        return if (Modifier.isStatic(f.modifiers)) f.getInt(null) else f.getInt(companionInstance)
     }
 }
