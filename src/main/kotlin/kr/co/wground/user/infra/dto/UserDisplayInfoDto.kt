60d57baa8762b6b944f667b252544fa884c2d025
dd-jiyun <dlwldbs8015@gmail.com>
2026-01-29 23:46:25 +0900

    [fix/scrum-209] 사용자 프로필 이미지 호출 메서드 변경

--- src/main/kotlin/kr/co/wground/user/infra/dto/UserDisplayInfoDto.kt
+++ src/main/kotlin/kr/co/wground/user/infra/dto/UserDisplayInfoDto.kt
@@ -1,10 +1,18 @@
 package kr.co.wground.user.infra.dto
 
 import kr.co.wground.global.common.UserId
+import kr.co.wground.user.utils.defaultimage.domain.UserProfile
 
 data class UserDisplayInfoDto(
     val userId: UserId,
     val name: String,
     val profileImageUrl: String,
     val trackName: String,
-)
+) {
+    constructor(userId: UserId, name: String, userProfile: UserProfile, trackName: String) : this(
+        userId = userId,
+        name = name,
+        profileImageUrl = userProfile.getAccessUrl(),
+        trackName = trackName
+    )
+}
