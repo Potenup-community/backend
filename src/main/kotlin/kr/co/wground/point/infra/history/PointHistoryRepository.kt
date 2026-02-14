30abc56c9c5f81818754ce07db71539c1ccf9310
dkstpwns10 <dkstpwns10@naver.com>
2026-02-15 01:13:24 +0900

    [fix/scrum-225] PointReferenceType으로 참조이름 변경

--- src/main/kotlin/kr/co/wground/point/infra/history/PointHistoryRepository.kt
+++ src/main/kotlin/kr/co/wground/point/infra/history/PointHistoryRepository.kt
@@ -3,14 +3,14 @@ package kr.co.wground.point.infra.history
 import kr.co.wground.global.common.UserId
 import kr.co.wground.point.domain.PointHistory
 import kr.co.wground.point.domain.PointType
-import kr.co.wground.point.domain.ReferenceType
+import kr.co.wground.point.domain.PointReferenceType
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface PointHistoryRepository : JpaRepository<PointHistory, Long>, CustomPointHistoryRepository {
 
     fun existsByUserIdAndRefTypeAndRefIdAndType(
         userId: UserId,
-        refType: ReferenceType,
+        refType: PointReferenceType,
         refId: Long,
         type: PointType
     ): Boolean
