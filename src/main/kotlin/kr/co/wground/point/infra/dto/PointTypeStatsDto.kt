0168c2b5ec64b51d9b6883506c9c76bfe2b45768
dkstpwns10 <dkstpwns10@naver.com>
2026-02-15 01:11:15 +0900

    [fix/scrum-225] Infra레이어의 Dto삭제

--- src/main/kotlin/kr/co/wground/point/infra/dto/PointTypeStatsDto.kt
+++
@@ -1,9 +0,0 @@
-package kr.co.wground.point.infra.dto
-
-import kr.co.wground.point.domain.PointType
-
-data class PointTypeStatsDto(
-    val type: PointType,
-    val count: Long,
-    val totalAmount: Long
-)
