7ad5a92a0f073e9b36ba2ba58abbb8d357eec8d2
SeungYeop-Han <ammezkhan@gmail.com>
2026-02-17 22:26:58 +0900

    refactor(study_schedule): 메서드 이름 변경

--- src/main/kotlin/kr/co/wground/study/application/StudyRecruitmentService.kt
+++ src/main/kotlin/kr/co/wground/study/application/StudyRecruitmentService.kt
@@ -48,7 +48,7 @@ class StudyRecruitmentService(
 
         val schedule = findScheduleByIdOrThrows(study.scheduleId)
         val requestMonth = scheduleRepository.findAllByTrackIdOrderByMonthsAsc(user.trackId)
-            .firstOrNull { it.isCurrentRound() } ?: throw BusinessException(StudyScheduleServiceErrorCode.SCHEDULE_NOT_FOUND)
+            .firstOrNull { it.isCurrentMonth() } ?: throw BusinessException(StudyScheduleServiceErrorCode.SCHEDULE_NOT_FOUND)
 
         recruitValidator.throwsWhenRecruitingIsOver(schedule)
         recruitValidator.throwsWhenScheduleIsNotCurrentMonth(schedule, requestMonth)
