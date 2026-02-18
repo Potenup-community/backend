fc19540c616d5dd5f6a57ca21ffffc4a9bddf60f
SeungYeop-Han <ammezkhan@gmail.com>
2026-02-19 00:33:35 +0900

    feat(user): 모든 운영진 조회를 위한 사용자 리포지토리 메서드 추가

--- src/main/kotlin/kr/co/wground/user/infra/UserRepository.kt
+++ src/main/kotlin/kr/co/wground/user/infra/UserRepository.kt
@@ -2,8 +2,10 @@ package kr.co.wground.user.infra
 
 import kr.co.wground.global.common.UserId
 import kr.co.wground.user.domain.User
+import kr.co.wground.user.domain.constant.UserRole
 import kr.co.wground.user.domain.constant.UserStatus
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface UserRepository : JpaRepository<User, Long>, CustomUserRepository {
     fun findByEmail(email: String): User?
@@ -11,4 +13,7 @@ interface UserRepository : JpaRepository<User, Long>, CustomUserRepository {
     fun countByStatus(status: UserStatus): Long
     fun findByUserIdIn(userIds: List<UserId>): List<User>
     fun existsByPhoneNumber(phoneNumber: String): Boolean
+
+    @Query("select u from User u where u.role = kr.co.wground.user.domain.constant.UserRole.ADMIN")
+    fun findAllByRole(): List<User>
 }
