01bb2b746bb253b920a5246d03083295e3303519
SeungYeop-Han <ammezkhan@gmail.com>
2026-02-18 20:45:51 +0900

    fix(study): 애플리케이션 재시작 시, 스터디 일정과 현재 시점간 순서와 해당 스터디의 상태 간 정합성이 깨질 수 있는 문제 해결
    이미 스터디 종료 시점이 지났으나, IN_PROGRESS 상태로 유지되고 있는 스터디의 상태를 COMPLETED 상태로 변경하거나,
    
    또는 이미 모집 마감 시점이 지났으나, RECRUITING 상태로 유지되고 있는 스터디의 상태를 RECRUITING_CLOSED 로 변경한다.

--- src/main/kotlin/kr/co/wground/global/scheduler/study/StudyScheduleStartupLoader.kt
+++ src/main/kotlin/kr/co/wground/global/scheduler/study/StudyScheduleStartupLoader.kt
@@ -1,5 +1,7 @@
 package kr.co.wground.global.scheduler.study
 
+import kr.co.wground.study.domain.enums.StudyStatus
+import kr.co.wground.study.infra.StudyRepository
 import kr.co.wground.study_schedule.infra.StudyScheduleRepository
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.event.ApplicationReadyEvent
@@ -12,6 +14,7 @@ import java.time.LocalDateTime
 @Component
 class StudyScheduleStartupLoader(
     private val studyScheduleRepository: StudyScheduleRepository,
+    private val studyRepository: StudyRepository,
     private val studyScheduleTaskManager: StudyScheduleTaskManager,
     @Value("\${spring.jpa.properties.hibernate.jdbc.batch_size:100}")
     private val batchSize: Int
@@ -26,11 +29,25 @@ class StudyScheduleStartupLoader(
         
         while (true) {
             val pageRequest = PageRequest.of(pageNumber, batchSize)
-            val schedules = studyScheduleRepository.findAllByStudyEndDateAfter(now, pageRequest)
+            val allSchedules = studyScheduleRepository.findAll()
 
-            if (schedules.isEmpty()) break
+            if (allSchedules.isEmpty()) break
 
-            schedules.forEach { schedule ->
+            allSchedules.forEach { schedule ->
+
+                // 이미 스터디 종료 시점이 지났으나, IN_PROGRESS 상태로 유지되고 있는 스터디의 상태를 COMPLETED 상태로 변경하거나
+                // 또는 이미 모집 마감 시점이 지났으나, RECRUITING 상태로 유지되고 있는 스터디의 상태를 RECRUITING_CLOSED 로 변경한다.
+                if (LocalDateTime.now().isAfter(schedule.studyEndDate)) {
+                    studyRepository.findAllByScheduleId(schedule.id)
+                        .filter { it.status == StudyStatus.IN_PROGRESS }
+                        .forEach { it.complete() }
+                } else if (LocalDateTime.now().isAfter(schedule.recruitEndDate)) {
+                    studyRepository.findAllByScheduleId(schedule.id)
+                        .filter { it.status == StudyStatus.RECRUITING }
+                        .forEach { it.closeRecruitment() }
+                }
+
+                // 이미 모집 일정이 마감되었거나, 스터디 일정이 종료된 스터디의 경우 태스크가 추가되지 않음
                 studyScheduleTaskManager.addTask(
                     scheduleId = schedule.id,
                     recruitStart = schedule.recruitStartDate,
