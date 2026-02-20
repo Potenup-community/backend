31c1566b103e099e2ba7953aea49bb44a7aa6e55
dd-jiyun <dlwldbs8015@gmail.com>
2026-02-20 16:41:47 +0900

    [refactor/scrum-231] 프로젝트 수정 API patch로 수정

--- src/main/kotlin/kr/co/wground/gallery/presentation/controller/ProjectController.kt
+++ src/main/kotlin/kr/co/wground/gallery/presentation/controller/ProjectController.kt
@@ -20,9 +20,9 @@ import org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RequestPart
