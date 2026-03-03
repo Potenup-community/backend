50779ccf63dfa9307691ae95d80ca392d1d2c1b3
SeungYeop-Han <ammezkhan@gmail.com>
2026-03-03 14:15:57 +0900

    hotfix(notification): conflict resolve 잘못했어요... 죄송합니다

--- src/main/kotlin/kr/co/wground/notification/domain/enums/NotificationType.kt
+++ src/main/kotlin/kr/co/wground/notification/domain/enums/NotificationType.kt
@@ -27,15 +27,8 @@ enum class NotificationType(
 
     // 멘션
     COMMENT_MENTION(SlackChannelCategory.GENERAL),
-  
-    // 공지사항
-    ANNOUNCEMENT,
-
-    // 스터디 모집
-    STUDY_RECRUIT_START,
-    STUDY_RECRUIT_END,
 
     //이력서 첨삭
-    RESUME_REVIEW_COMPLETED
+    RESUME_REVIEW_COMPLETED(SlackChannelCategory.GENERAL)
     ;
 }
