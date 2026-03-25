a4de608d44a08f0ae3bbe1d7aab91401ac890d39
AN <sejun@ANui-MacBookAir.local>
2026-03-25 16:00:27 +0900

    feat(auth): JWT/Principal에 세션 ID 클레임 연동

--- src/main/kotlin/kr/co/wground/global/jwt/UserPrincipal.kt
+++ src/main/kotlin/kr/co/wground/global/jwt/UserPrincipal.kt
@@ -3,6 +3,7 @@ package kr.co.wground.global.jwt
 import kr.co.wground.global.common.UserId
 
 data class UserPrincipal(
-    val userId : UserId,
-    val role : String
+    val userId: UserId,
+    val role: String,
+    val sessionId: String? = null,
 )
